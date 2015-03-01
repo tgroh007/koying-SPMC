27bdc8b9ac5b1c7018c53d094f2317c91e40fe17
Chris "Koying" Browet <cbro@semperpax.com>
2015-03-01 20:22:49 +0100

    FIX: [droid] when a videodb intent is passed, fill video tag

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -89,6 +89,7 @@
 
 #include "CompileInfo.h"
 #include "video/videosync/VideoSyncAndroid.h"
+#include "filesystem/VideoDatabaseFile.h"
 
 #define GIGABYTES       1073741824
 
@@ -877,8 +878,11 @@ void CXBMCApp::onNewIntent(CJNIIntent intent)
   std::string action = intent.getAction();
   if (action == "android.intent.action.VIEW")
   {
-    CApplicationMessenger::GetInstance().SendMsg(TMSG_MEDIA_PLAY, 1, 0, static_cast<void*>(
-                                         new CFileItem(GetFilenameFromIntent(intent))));
+    std::string playFile = GetFilenameFromIntent(intent);
+    CFileItem* item = new CFileItem(playFile, false);
+    if (item->IsVideoDb() && !item->HasVideoInfoTag())
+      *(item->GetVideoInfoTag()) = XFILE::CVideoDatabaseFile::GetVideoTag(CURL(item->GetPath()));
+    CApplicationMessenger::GetInstance().PostMsg(TMSG_MEDIA_PLAY, 0, 0, static_cast<void*>(item));
   }
 }
 
