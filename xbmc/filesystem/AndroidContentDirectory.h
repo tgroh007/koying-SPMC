8c6c81d4790a974ccb7588b048c44d82ecef6d52
Chris "Koying" Browet <cbro@semperpax.com>
2016-01-10 16:34:15 +0100

    [WIP] android content VFS

--- xbmc/filesystem/AndroidContentDirectory.h
+++ xbmc/filesystem/AndroidContentDirectory.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2014 Team XBMC
+ *      Copyright (C) 2012-2013 Team XBMC
  *      http://xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
@@ -18,39 +18,24 @@
  *
  */
 
-#include "CompileInfo.h"
-#include <cstddef>
-
-
-int CCompileInfo::GetMajor()
+#pragma once
+#if defined(TARGET_ANDROID)
+#include "IDirectory.h"
+#include "FileItem.h"
+namespace XFILE
 {
-  return @APP_VERSION_MAJOR@;
-}
 
-int CCompileInfo::GetMinor()
+class CAndroidContentDirectory :
+      public IDirectory
 {
-  return @APP_VERSION_MINOR@;
+public:
+  CAndroidContentDirectory(void);
+  virtual ~CAndroidContentDirectory(void);
+  virtual bool GetDirectory(const CURL& url, CFileItemList &items);
+  virtual bool Exists(const CURL& url) { return true; };
+  virtual bool AllowAll() const { return true; };
+  virtual DIR_CACHE_TYPE GetCacheType(const std::string& strPath) const { return DIR_CACHE_NEVER; }
+};
 }
-
-const char* CCompileInfo::GetAppName()
-{
-  return "@APP_NAME@";
-}
-
-const char* CCompileInfo::GetSuffix()
-{
-  return "@APP_VERSION_TAG@";
-}
-
-const char* CCompileInfo::GetSCMID()
-{
-  return "@APP_SCMID@";
-}
-
-void CJNIActivity::startActivityForResult(const CJNIIntent &intent, int requestCode)
-{
-  call_method<void>(jhobject(m_context),
-    "startActivityForResult", "(Landroid/content/Intent;I)V",
-    intent.get_raw(), requestCode);
-}
+#endif
 
